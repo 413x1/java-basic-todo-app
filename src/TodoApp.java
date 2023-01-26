ab6f149401dc79c50cce72fac879c1ae308c84af
413x1 <muhammad.sholeh@sirclo.com>
2023-01-26 12:02:20 +0700

    create todo array and operation

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -1,5 +1,19 @@
 public class TodoApp {
+
+    public static String[] data = new String[10];
     public static void main(String[] args) {
+
+    }
+
+    public static void showTodo() {
+
+    }
+
+    public static void addTodo() {
+
+    }
+
+    public static void deleteTodo() {
 
     }
 }
