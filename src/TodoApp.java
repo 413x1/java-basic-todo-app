48d278b3359d0b76fcde9513412fb91468d0d0b4
413x1 <muhammad.sholeh@sirclo.com>
2023-01-26 12:08:24 +0700

    add showTodo logic

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -6,7 +6,15 @@ public class TodoApp {
     }
 
     public static void showTodo() {
+        for(var i=0; i< data.length; i++) {
+            var todo = data[i];
+            var no = i+1;
 
+            if (todo != null) {
+                System.out.println(no + ". "+ todo);
+            }
+
+        }
     }
 
     public static void addTodo() {
@@ -14,6 +22,18 @@ public class TodoApp {
     }
 
     public static void deleteTodo() {
+
+    }
+
+    public static void viewShowTodo() {
+
+    }
+
+    public static void viewAddTodo() {
+
+    }
+
+    public static void viewDeleteTodo() {
 
     }
 }
